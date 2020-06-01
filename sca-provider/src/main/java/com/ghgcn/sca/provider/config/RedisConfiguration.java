462e4a394ae34cb44b6530d49f55c7e97ef446d5
liunan <153011784@qq.com>
2020-06-01 15:12:28 +0800

    add file.conf registry.conf config.txt

---
+++ sca-provider/src/main/java/com/ghgcn/sca/provider/config/RedisConfiguration.java
@@ -0,0 +1,21 @@
+//package com.ghgcn.sca.provider.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//测试
+//@Configuration
+//public class RedisConfiguration {
+//
+////    @Bean
+////    public JedisConnectionFactory redisConnectionFactory() {
+////
+////        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+////                .master("mymaster")
+////                .sentinel("127.0.0.1", 26379)
+////                .sentinel("127.0.0.1", 26380);
+////        return new JedisConnectionFactory(sentinelConfig);
+////    }
+//}
